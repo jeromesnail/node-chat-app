afb817e017c8f7053a4b6414a1b996fbd1e16078
jeromesnail <jeromesnail@hotmail.fr>
2018-03-31 17:48:19 +0200

    Added location

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,42 @@ socket.on('disconnect', () => {
 });
 
 socket.on('newMessage', (message) => {
-  console.log(`Message from ${message.from}:`, message.text); 
   const li = jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
   jQuery('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', (e) => {
+socket.on('newLocationMessage', location => {
+  const li = jQuery('<li></li>');
+  const a = jQuery('<a target="_blank">My current location</a>');
+  li.text(`${from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', e => {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
     text: jQuery('[name=message]').val()
   }, () => {
-    
+
+  });
+});
+
+
+const locationButton = jQuery('#send-location');
+locationButton.on('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your brother');
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })     
+  }, () => {
+    alert('Unable to get location');
   });
 });
