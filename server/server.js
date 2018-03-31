7721ef8ad4d89b6fb8cd0882a756f5d5699b7339
jeromesnail <jeromesnail@hotmail.fr>
2018-03-31 10:31:08 +0200

    Fixed port for heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,6 @@ const publicPath = path.join(__dirname, '/../public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}...`);  
 })
