d9eccb79a69a5b10dfe673a4d3e6066223710067
jeromesnail <jeromesnail@hotmail.fr>
2018-04-01 12:44:50 +0200

    Added test for generateLocationMessage

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -9,5 +9,17 @@ describe('generateMessage', () => {
     const message = generateMessage(from, text);
     expect(message).toMatchObject({ from, text });
     expect(typeof message.createdAt).toBe('number');
+  });
+});
+
+describe('generate location', () => {
+  it('should generate correct location object', () => {
+    const from = 'Proutlala';
+    const lat = 4.236;
+    const long = 9.3215;
+    const url = 'https://www.google.com/maps?q=4.236,9.3215';
+    const location = generateLocationMessage(from, lat, long);
+    expect(location).toMatchObject({ from, url });
+    expect(typeof location.createdAt).toBe('number');
   });
 });
